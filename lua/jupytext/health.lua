7a2f5b30277cdb78091871d76e89abc693fffa53
GCBallesteros <dev+github@maxwellrules.com>
2023-11-22 21:45:16 +0200

    feat: Add healthcheck

---
+++ lua/jupytext/health.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.check = function()
+  vim.health.report_start "jupytext.nvim"
+  vim.fn.system "which jupytext"
+
+  if vim.v.shell_error == 0 then
+    vim.health.report_ok "Jupytext is available"
+  else
+    vim.health.report_error("Jupytext is not available", "Install jupytext via `pip install jupytext`")
+  end
+end
+
+return M
