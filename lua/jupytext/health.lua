d30aca899430b40d164c232985ccd0d7ca7f01c8
gogamid <36050790+gogamid@users.noreply.github.com>
2024-06-02 23:49:16 +0200

    fix: replace depricated methods

--- lua/jupytext/health.lua
+++ lua/jupytext/health.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.check = function()
-  vim.health.report_start "jupytext.nvim"
+  vim.health.start "jupytext.nvim"
   vim.fn.system "jupytext --version"
 
   if vim.v.shell_error == 0 then
-    vim.health.report_ok "Jupytext is available"
+    vim.health.ok "Jupytext is available"
   else
-    vim.health.report_error("Jupytext is not available", "Install jupytext via `pip install jupytext`")
+    vim.health.error("Jupytext is not available", "Install jupytext via `pip install jupytext`")
   end
 end
 
